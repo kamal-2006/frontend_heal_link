ba2f07001ea259cf299201835a6d3f67a8dcc9ec
kamal-2006 <kamaleshkamales006@gmail.com>
2025-09-19 20:26:04 +0530

    done google signin

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import GoogleProvider from "../components/GoogleProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <GoogleProvider>
+          {children}
+        </GoogleProvider>
       </body>
     </html>
   );
---
+++ src/components/GoogleProvider.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+export default function GoogleProvider({ children }) {
+  const clientId = (process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "").trim();
+  if (!clientId) return children;
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      {children}
+    </GoogleOAuthProvider>
+  );
+}
+
+
